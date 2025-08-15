f88e305ffc1d321a24ed429408e47be15874370e
Teguh Bagas Mardiansyah <116806781+TeguhBagasM@users.noreply.github.com>
2025-08-15 09:22:39 +0300

    initial commit

---
+++ database/migrations/2025_08_15_061611_create_customers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+			$table->string('nama_customer');
+			$table->string('kode_customer');
+			$table->string('alamat_customer');
+			$table->string('telepon_customer');
+
+            $table->timestampSoftDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
