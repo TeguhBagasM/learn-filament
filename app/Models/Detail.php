b80567a425e75716d985f6363a32245906f5bdb0
Teguh Bagas Mardiansyah <116806781+TeguhBagasM@users.noreply.github.com>
2025-08-15 10:41:59 +0300

    feat: add relationship faktur and detail

--- app/Models/Detail.php
+++ app/Models/Detail.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Detail extends Model
 {
-    //
+    use HasFactory;
+    protected $guarded = [];
+
+    public function barang()
+    {
+        return $this->belongsTo(Barang::class);
+    }
+
+    public function faktur()
+    {
+        return $this->belongsTo(Faktur::class, 'id');
+    }
+
 }
--- app/Models/Faktur.php
+++ app/Models/Faktur.php
@@ -2,9 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Faktur extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $guarded = [];
+
+    public function customer()
+    {
+        return $this->belongsTo(CustomerModel::class);
+    }
+
+    public function detail()
+    {
+        return $this->hasMany(Detail::class, 'faktur_id');
+    }
 }
